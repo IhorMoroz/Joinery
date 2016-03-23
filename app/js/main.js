099dc065885292e2da3b4563b75c88a38e62ec28
IhorMoroz <work.ihor.m@gmail.com>
2016-03-23 21:48:02 +0200

    make corect scroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
+$(document).ready(function(){
+    var heightStaticBlock = 67;
+    $('a[href^="#"]').click(function(e){
+        e.preventDefault();
+        var target = this.hash,
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - heightStaticBlock
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
+
 var map,
     marker,
     position = {
